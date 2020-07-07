8f2c764eee2d23daf91d66eb0af941d08520e90e
Encke Technologies <james@encke.com>
2020-07-07 14:00:43 -0600

    Update to bcrypt to return error

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ module.exports = {
 		return module.exports.crypto.createHash( "sha1" ).update( value ).digest( "hex" );
 	},
 	bcryptCreate: ( plainTextPassword, saltRounds, callback ) => bcrypt.genSalt( ( saltRounds? saltRounds: 10 ), ( err, salt ) => bcrypt.hash( plainTextPassword, salt, ( err, hash ) => callback( hash ) ) ),
-	bcryptCompare: ( plainTextPassword, encryptedPassword, callback ) => bcrypt.compare( plainTextPassword, encryptedPassword.replace( '$2y$', '$2a$' ), ( err, correct ) => callback( correct ) ),
+	bcryptCompare: ( plainTextPassword, encryptedPassword, callback ) => bcrypt.compare( plainTextPassword, encryptedPassword, ( err, correct ) => callback( correct, err ) ),
 	isoDatetime: ( start ) => {
 		let thisDateString = "";
 		try	{
